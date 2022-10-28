55ca93536c8890280c34b2675d2a5debcd290a88
gitcuamanhne-ahihi <shuriken8668@gmail.com>
2022-10-29 00:24:55 +0700

    update preview close icon

--- src/components/common/PreviewImage.tsx
+++ src/components/common/PreviewImage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaTimes } from './Icon'
-import { useAppDispatch, useAppSelector } from '../../hooks'
+import { useAppDispatch, useAppSelector, useCheckpoint } from '../../hooks'
 import { overlay } from '../../sliceredux';
 import { close } from '../../sliceredux/preview-image.slice';
 import '../../styles/preview-image.scss'
@@ -10,6 +10,7 @@ type Props = {}
 export  function PreviewImage({ }: Props) {
   const { open: openmodal, url: urlimage } = useAppSelector(state => state.previewimagemodal)
   const dispatch = useAppDispatch();
+  const { deviceCurrent } = useCheckpoint('');
   const closePreview = () => {
     dispatch(close());
     dispatch(overlay());
@@ -21,7 +22,7 @@ export  function PreviewImage({ }: Props) {
         <div className="preview-image">
           <div className="preview-image__header">
             <div className="preview-image__close" onClick={()=>closePreview()}>
-              <FaTimes size={25} className="preview-image__close-icon" />
+              <FaTimes size={deviceCurrent === 'mobile'? 15 : 25} className="preview-image__close-icon" />
             </div>
           </div>
           <div className="preview-image__image" style={{background: `url(${urlimage})`}}>
